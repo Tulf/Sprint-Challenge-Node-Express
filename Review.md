638febafa1fa1b90466ebb2932ff3be59ac2d4c3
tulf <timzulf@gmail.com>
2018-09-07 11:40:41 -0400

    Tim Zulf MBP- Review.md done Get all actions Actions by id Projects Proejcts by id

--- Review.md
+++ Review.md
@@ -1,17 +1,19 @@
 # Review Questions
 
 ## What is Node.js?
-
+Node.js is an open  source server environment which can handle API requests to communicate between websites and servers.
+It runs asynchronously.
 ## What is Express?
-
+Express is a web framework for Node.js it has middleware API calls and all kinds of web framework features.
 ## Mention two parts of Express that you learned about this week.
-
+Routing and middleware support. We've used middleware like cors and body-parser
 ## What is Middleware?
-
+middleware is generally like pieces of code that sit in the middle of various processes, but in this context, middleware can interrupt http requests to perform some action (like transform elements within an array) and pass along to either the next middleware or let the request get fulfilled. 
 ## What is a Resource?
-
+In the REST architectural style everything is a resource accessible through a unique URI.
 ## What can the API return to help clients know if a request was successful?
-
+Usually the success of this process is self evident but an api can return a successful response if necessary.
 ## How can we partition our application into sub-applications?
-
+You can mount other express apps with .use()
 ## What is express.json() and why do we need it?
+To be able to read data from the request body because otherwise express wouldn't add objects to the body.
---
+++ server.js
@@ -0,0 +1,258 @@
+const express = require('express');
+const server = express();
+const actiondb = require("./data/helpers/actionModel.js")
+const projectdb = require("./data/helpers/projectModel.js")
+const cors = require("cors");
+
+
+
+
+//middleware
+server.use(express.json())
+
+// function logger(req, res, next) {
+//   console.log(`${req.method} to ${req.url}`);
+//
+//   next(); //calls the next middleware in the queue
+// }
+
+
+// for all
+function userToUpperCase(req, res, next) {
+  req.monkey = true
+  next();
+}
+//for a particular user
+function capatlize( req, res, next){
+  req.body.name = req.body.name.toUpperCase();
+  next();
+}
+
+
+//ask eric why you need req.monkey and note just a declared bolean value?
+
+/*
+reusable bit of captalization:
+if(req.monkey) {users = users.map(each =>
+    each.name.toUpperCase())
+*/
+
+//----------------routes: ------------------------//
+
+// initial
+server.get('/', (req, res) => {
+  res.send('Hello')
+})
+
+// --Get all actions
+server.get('/actions',  (req, res) => {
+
+    const id = req.params.id;
+    actiondb.get(id)
+    .then(users => {
+      if(users){
+            res.status(200).json(users)
+          }
+            else{
+              res.status(404).json({error: "could not get actions"})
+            }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({error: 'Cannot Get actions'})
+    })
+})
+
+//actions by id:
+server.get('/actions/:id',  (req, res) => {
+
+    const id = req.params.id;
+    actiondb.get(id)
+    .then(users => {
+      if(users){
+            res.status(200).json(users)
+          }
+            else{
+              res.status(404).json({error: "could not get actions"})
+            }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({error: 'Cannot Get actions'})
+    })
+})
+
+//get all projects
+server.get('/projects/',  (req, res) => {
+
+    const id = req.params.id;
+    projectdb.get(id)
+    .then(users => {
+      if(users){
+            res.status(200).json(users)
+          }
+            else{
+              res.status(404).json({error: "Could not find projects"})
+            }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({error: 'Cannot Get projects'})
+    })
+})
+
+//get all projects by id
+server.get('/projects/:id',  (req, res) => {
+
+    const id = req.params.id;
+    projectdb.get(id)
+    .then(users => {
+      if(users){
+            res.status(200).json(users)
+          }
+            else{
+              res.status(404).json({error: "project does not exist"})
+            }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({error: 'Cannot Get projects'})
+    })
+})
+
+// //posts
+// server.get('/posts', (req, res) => {
+//
+//     const id = req.params.id;
+//     postdb.get(id)
+//     .then(users => {
+//         res.status(200).json(users)
+//     })
+//     .catch(err => {
+//       console.log(err)
+//       res.status(500).json({error: 'Cannot Get Posts'})
+//     })
+// })
+// //posts by post id, cannot seem to do it by userId this way.
+// server.get('/posts/:id', (req, res) => {
+//
+//     const id = req.params.id;
+//     postdb.get(id)
+//     .then(posts => {
+//       if(posts){
+//         res.status(200).json(posts)
+//       }
+//       else{
+//         res.status(404).json({error: "do it right moron"})
+//       }
+//     })
+//     .catch(err => {
+//       console.log(err)
+//       res.status(500).json({error: 'Cannot Get Posts'})
+//     })
+// })
+// // --Get User By id
+// server.get('/users/:id',  (req, res) => {
+//
+//     const id = req.params.id;
+//
+//     userdb.get(id)
+//     .then(users => {
+//       if(users){
+//
+//         res.status(200).json(users)
+//       }
+//       else{
+//         res.status(404).json({error: "do it right moron"})
+//       }
+//     })
+//     .catch(err => {
+//       console.log(err)
+//       res.status(500).json({error: 'Cannot Get Users'})
+//     })
+// })
+// //--Get post for a given user:
+// server.get('/users/posts/:id', (req,res) => {
+//
+//   const id = req.params.id;
+//
+//   userdb.getUserPosts(id)
+//     .then(user => {
+//       if(user.length === 0){
+//         res.status(400).json({message: 'This id could not be found'})
+//       }
+//       else{
+//         res.status(200).json(user)
+//       }
+//     })
+//     .catch(err => {
+//       console.log(err)
+//       res.status(404).json({error: "failed to get user"})
+//     })
+// })
+// //--Add user through Post
+// server.post('/users/:id', capatlize, (req, res) => {
+//
+//   const username = req.body.name;
+//   if(!username){
+//     res.status(400).json({message: "Please provide a name for this user"})
+//   }
+//   userdb.insert(req.body)
+//   .then(user => {
+//     res.status(200).json(user)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//     res.status(500).json({error: "Failed to add user"})
+//   })
+// })
+// //getting sql  constraint for duplicate names how to  handle this exception?
+// // How to add with specified route id?
+//
+// //Delete User
+// server.delete('/users/:id', (req, res) => {
+//
+//   const id = req.params.id;
+//   userdb.remove(id)
+//   .then(user => {
+//     if(user === 0) {
+//       res.status(400).json({ message: "please use a valid id"})
+//     }
+//     else{
+//       res.status(200).json(user)
+//     }
+//     })
+//     .catch(err =>{
+//       console.log(err)
+//       res.status(500).json({ error: "can't delete user" })
+//   })
+// })
+//
+//
+// //Update User
+// server.put('/users/:id', capatlize, (req, res) =>{
+//
+//   const id = req.params.id;
+//   const name = req.body.name;
+//   const body = req.body
+//
+//   if(!name){
+//     res.status(400).json({error: "Please christen this poor virtual soul"})
+//   }
+//
+//   userdb.update(id, body)
+//   .then(user => {
+//     if(user === 0){
+//       res.status(400).json({message: 'This Id does not exist'})
+//     }
+//     else{
+//       res.status(200).json(user)
+//     }
+//   })
+//   .catch(err => {
+//     console.log(err)
+//     res.status(500).json({message: "Failed to update user, please discipline database accordingly"})
+//   })
+// })
+//Listener
+server.listen(8000, ( ) => console.log('\n == API on port 8000 =='))
